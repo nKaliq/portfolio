24d0c3832604d5f8173590f4536f3e9ffb9561ae
Nitin Kumar <37310198+nKaliq@users.noreply.github.com>
2020-01-15 22:34:10 +0530

    Glass Design Tryout

---
+++ script.js
@@ -0,0 +1,9 @@
+function showFeed()
+{
+  document.getElementById("feed_div").style.visibility="visible";
+}
+
+function hideFeed()
+{
+  document.getElementById("feed_div").style.visibility="hidden";
+}
